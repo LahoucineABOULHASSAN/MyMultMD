acdb86842b7e194dee6c02d9f80fe3441d9d2cbd
LahoucineABOULHASSAN <em.aboulhassan@gmail.com>
2021-04-10 16:47:48 +0100

    update image resources

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
 // get the DOM items
-const playBtn = document.querySelector(".huge");
 const topNav = document.querySelector(".topnav");
 const responsiveBtn = document.querySelector(".responsive-btn");
-// Event Listeners
-playBtn.addEventListener("click", togglePlayPause);
 // menun bar code responsiveness
-function togglePlayPause() {
-  playBtn.classList.toggle("play");
-  playBtn.classList.toggle("pause");
-}
 function displayMenu() {
   topNav.classList.toggle("responsive");
   responsiveBtn.classList.toggle("times");
